b9157ecb39358b67d66f0d3787535404a644e1ef
danielwadhi <angeluswadhi1306@gmail.com>
2024-05-28 12:07:59 +0700

    commit pertama

---
+++ iseng.java
@@ -0,0 +1,17 @@
+public class PerkenalanDiri {
+    public static void main(String[] args) {
+        // Variabel dengan tipe data String untuk nama
+        String nama = "John Doe";
+
+        // Variabel dengan tipe data integer untuk umur
+        int umur = 25;
+
+        // Variabel dengan tipe data float untuk tinggi badan
+        float tinggiBadan = 175.5f;
+
+        // Menampilkan perkenalan diri menggunakan variabel-variabel di atas
+        System.out.println("Halo, nama saya " + nama + ".");
+        System.out.println("Saya berumur " + umur + " tahun.");
+        System.out.println("Tinggi badan saya adalah " + tinggiBadan + " cm.");
+    }
+}
